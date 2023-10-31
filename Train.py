2fa1a42b7fb164860fe94eccbe02e352b254486a
Yansirui <118953128+Yansirui@users.noreply.github.com>
2023-10-31 13:13:29 +0800

    Update Train.py

--- Train.py
+++ Train.py
@@ -7,6 +7,7 @@ import torch
 import wandb
 import argparse
 from torch import nn
+
 def train(batch_size,learn):
     #name1=check_point
     batch_size = batch_size
@@ -52,9 +53,13 @@ def train(batch_size,learn):
             batch = {k: v.to(device) if torch.is_tensor(v) else v for k, v in batch.items()}
             #print(batch['cl_input_ids'].shape)
             outputs=model(input_ids=batch['cl_input_ids'],attention_mask =batch['cl_attention_mask'],token_type_ids=batch['cl_token_type_ids'])
+            #2是因为选择2个step更新一次参数，所以除以二防止loss过大引起不好的效果
             loss = outputs.loss / 2
             loss.backward()
             wandb.log({'loss':loss})
+            #显存不够时候可以适当增大2，如果显存足够，那就是1个step更新1次，改为1即可，但是针对于该方法而言
+            #比如batch_size为8，我想要实现batch_size为16的效果是不可能的，因为batch_size=8时进分类层的维度为[8,16]，也就是每个句子从16个句子中找到positive，然而batch_size=16为[16,32]
+            #这种2个step更新一次的方法只是做到了[16,16]而不是[8,32]
             if step_num % 2==0 and step_num!=0:
                 optimizer.step()
                 lr_scheduler.step()
